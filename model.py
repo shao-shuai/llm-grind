d754c379c6cb4d2604c0497649081ef2eb9523e1
shao-shuai <shuai-shao@outlook.com>
2025-03-10 21:35:07 -0700

    added part of GPT class

--- model.py
+++ model.py
@@ -116,3 +116,141 @@ class FeedForward(nn.Module):
 
         def forward(self, x):
             return self.dropout(self.w2(F.silu(self.w1(x)) * self.w3(x)))
+        
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.RMSNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.RMSNorm(config.n_embed)
+        self.ffd = FeedForward(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.ffd(self.ln_2(x))
+
+        return x
+    
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        # Create base transformer componetns
+        transformer_dict = {
+            "wte": nn.Embedding(config.vocab_size, config.n_embed),
+            "drop": nn.Droput(config.dropout),
+            "h": nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            "ln_f": nn.RMSNorm(config.n_embed)            
+        }
+
+        # Only add positional embeddings if not using rotary
+        if not config.use_rotary:
+            transformer_dict["wpe"] = nn.Embedding(config.block_size, config.n_embed)
+
+        self.transformer = nn.ModuleDict(transformer_dict)
+
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+
+        for pn, p in self.named_parameters():
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
+                )
+
+    def __init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        x = self.transformer.wte(idx)
+
+        # Add learnable positional embeddings
+        if not self.config.use_rotary:
+            device = idx.device
+            b, t = idx.shape
+            pos_emb = self.transformer.wpe(
+                torch.arange(0, t, dtype=torch.long, device=device)
+            )
+            x = x + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.shape[-1]), targets.view(-1), ignore_idnex=-1
+            )
+        else:
+            logits = self.lm_head(x[:, [-1], :])
+            loss = None
+
+        return logits, loss
+    
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        param_dict = {pn: p for pn, p in self.named_buffers()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
+        )
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = "fused" in inspect.isgnature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas, **extra_args
+        )
+        print(f"using fused AdamW: {use_fused}")
+
+        return optimizer
+    
+    @torch.no_grad()
+    def generate(
+        self, idx, max_new_tokens, temperature=1.0, top_k=None, top_p=None, min_p=None
+    ):
+        for _ in range(max_new_tokens):
+            context = (
+                idx
+                if idx.size(1) < self.config.blogck_size
+                else idx[:, -self.config.block_size :]
+            )
+            logits, _ = self(context)
+
+            logits = logits[:, -1, :] / temperature
+
+            if top_p is not None and top_p > 0.0:
+                probs = torch.softmax(logits, dim=-1)
+                sorted_probs, sorted_indices = torch.sort(
+                    probs, descending=True, dim=-1
+                )
+                cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+                mask = cumulative_probs >= top_p
+                mask[..., 0] = True
+
+                cutoff_indices = mask.int().argmax(dim=-1, keepdim=True)
+
+                top_p_mask = torch.zeros_like(logits, dtype=torch.bool)
+
